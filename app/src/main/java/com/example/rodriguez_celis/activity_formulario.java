357e0cddca1c1b0cabf6edaed4f75311c41b5e3d
abaza2022 <104527120+abaza2022@users.noreply.github.com>
2023-04-12 14:37:21 -0500

    formulario

---
+++ app/src/main/java/com/example/rodriguez_celis/activity_formulario.java
@@ -0,0 +1,14 @@
+package com.example.rodriguez_celis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_formulario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+    }
+}
